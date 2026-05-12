e69d4b3af332df2f4f1871b8d9e2e659f8355056
Abdielitis67 <meirdanz4@gmail.com>
2026-05-12 11:08:11 +0700

    Create stack constructor

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -12,3 +12,65 @@ public:
     Node()
     {
         next = NULL;
+    }
+};
+
+//Stack class
+class stack
+{
+private:
+    Node *top;
+
+public:
+    stack()
+    {
+        top = NULL;
+    }
+    int push(int value)
+    {
+        Node *newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+        cout << "Push value: " << value << endl;
+        return value;
+    }
+
+    void pop()
+    {
+        if (isEmpty())
+        {
+            cout << "Stack is empty" << endl;
+        }
+
+        Node *temp = top;
+        top = top->next;
+        cout << "Pop value: " << temp->data << endl;
+    }
+
+    //peel/top operation: Retrive the value of the topest element without removing
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is Empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " ";
+                current = current->next;
+            }
+            cout << endl;
+
+        }//return top->data;
+    }
+
+    //isEmpty operation: Check if the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+};
