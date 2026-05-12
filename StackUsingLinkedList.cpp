c96d92a879edf27e47966d6a1c2a70607ca0b692
Abdielitis67 <meirdanz4@gmail.com>
2026-05-12 11:18:44 +0700

    Add user input for push value

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -67,3 +67,65 @@ public:
 
         }//return top->data;
     }
+
+    //isEmpty operation: Check if the stack is empty
+    bool isEmpty()
+    {
+        return top == NULL;
+    }
+};
+
+
+int main()
+{
+    stack s;
+    int choice = 0;
+    int value;
+    
+while (choice !=5)
+{
+    cout << "1.push\n";
+    cout << "2.pop\n;
+    cout << "3.peek\n;
+    cout << "4.exit\n" << endl;
+    cin >> choice;
+
+    switch (choice)
+    {
+    case 1:
+        cout << "Enter value to push: ";
+        cin >> value;
+        s.push(value);
+        break;
+    case 2:
+        if (!stack.isEmpty())
+        {            
+            s.pop();
+        }
+        else
+        {
+            cout << "Stack is empty" << endl;
+        }
+        break;
+    case 3:
+        if (!stack.isEmpty())
+        {
+            s.peek();
+        }
+        else
+        {
+            cout << "Stack is empty" << endl;
+        }
+        break;
+
+    case 4:
+        cout << "Exiting..." << endl;
+        break;
+    default:
+        cout << "Invalid choice!" << endl;
+        break;
+    }
+    cout << endl;
+}
+return 0;
+};
